c4f8f66bad2ad27d5bf93644ca461e48c5907893
amine-54 <m.manyani0@gmail.com>
2026-09-09 14:37:53 +0800

    1st push

---
+++ ex2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	int number = 2024;
+	const char* txt = " hello hh ";
+
+	std::cout << txt << number;
+}
---
+++ tst.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define PI 3.14152926
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	cout<<a+PI;
+}
+
